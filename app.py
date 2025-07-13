02bc6ddceea843d638b070266448aec739d15b85
nikhil0523 <74248467+nikhil0523@users.noreply.github.com>
2025-07-14 03:45:02 +0530

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Amazon Reviews", layout="wide")
-st.title("📦 Amazon Reviews Topic & Sentiment Explorer")
-st.info("Upload your Sent.csv and images folder to explore reviews.")
